82386c9955a14034bed2ac2f0ae9a1945252fc6c
Sahil Bansal <bansalsahil315@gmailcom>
2024-05-03 21:56:26 +0530

    made changes in dotenv

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import express from "express";
 import { userRouter } from "./routers/user.router.js";
 import { connectDB } from "./db/connect.db.js";
 import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 import { blogRouter } from "./routers/blog.router.js";
 
 const app = express();
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: "sahil9181",
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,12 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import { CLOUDINARY_KEY, CLOUDINARY_SECRET_KEY } from "../utils/config.js";
-
-cloudinary.config({
-  cloud_name: "sahil9181",
-  api_key: CLOUDINARY_KEY,
-  api_secret: CLOUDINARY_SECRET_KEY,
-});
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
